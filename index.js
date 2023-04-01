952b5f77de27d2056f1b57c2c9f219fefc44b040
AliciaWu <aliciawu0702@gmail.com>
2023-04-01 18:13:23 +0800

    bot 1

---
+++ index.js
@@ -0,0 +1,81 @@
+const { Client, GatewayIntentBits, AttachmentBuilder, EmbedBuilder, Embed  } = require('discord.js');
+const { token, id } = require('./setting.json')
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  const number = Math.floor(Math.random()*100);
+
+  switch(interaction.commandName){
+    case '夭夭打招呼':
+        const HiAry =['去鼠la','請去你ㄇㄉ','你啥小','敲理嗎']
+        await interaction.reply(HiAry[(number%4)]);
+    break;
+
+    case '夭夭放技能':
+    const skillAry = ['奔褲衝', '鎖擒褲', '破褲','絕對守褲']
+    await interaction.reply(skillAry[(number%4)]);
+    break;
+
+    case '海夭占卜':
+        const sayAry = ['你今天會衰', '小心被狗咬褲子', '你好像有點好運','天吶~你出運了!','你頭上一個字『危』','大吉大利今晚吃雞','小心上廁所沒衛生紙']
+        await interaction.reply(sayAry[(number%7)]);
+        break;
+
+    case '夭夭抽卡':
+        const i = (number%11) == 0 ? 1 : (number%11);
+        const attachment = new AttachmentBuilder('./src/img/'+ i +'.png', { name: i+'.png' })
+        const embed = new EmbedBuilder()
+        .setTitle('你去鼠la')
+        await interaction.reply({embeds: [embed], files: [attachment]})
+        
+    break;
+
+    case '萬褲歸一':
+        await interaction.reply('萬褲歸一');
+    break;
+
+    case '一褲成神':
+        await interaction.reply('一褲成神');
+    break;
+
+    case '千褲破':
+        await interaction.reply('千褲破');
+    break;
+
+    case '破褲':
+        await interaction.reply('破褲');
+    break;
+
+    case '殺褲':
+        await interaction.reply('殺褲');
+    break;
+
+    case '褲致命':
+        await interaction.reply('褲致命');
+    break;
+
+    case '褲囚':
+        await interaction.reply('褲囚');
+    break;
+
+    case '死亡狙褲':
+        await interaction.reply('死亡狙褲');
+    break;
+
+    case '澄空略褲':
+        await interaction.reply('澄空略褲[擊倒並搶走褲子]');
+    break;
+
+    case '褲池領域':
+        await interaction.reply('褲池·領域');
+    break;
+  }
+
+});
+
+client.login(token);
---
+++ src/commands/tools/deploy.js
@@ -0,0 +1,77 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const { token, id } = require('../../../setting.json');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+//#region old
+const commands = [
+    {
+        name: '夭夭打招呼',
+        description: '海夭不屑的說',
+    },
+    {
+        name: '夭夭放技能',
+        description: '看我的厲害~你們都去鼠',
+    },
+    {
+        name: '海夭占卜',
+        description: '來看看你今天運勢阿',
+    },
+    {
+        name: '夭夭抽卡',
+        description: '翻開覆蓋的卡',
+    },
+    {
+        name: '萬褲歸一',
+        description: '夭夭說:「萬褲歸一」',
+    },
+    {
+        name: '一褲成神',
+        description: '夭夭說:「一褲成神」',
+    },
+    {
+        name: '千褲破',
+        description: '夭夭說:「千褲破」',
+    },
+    {
+        name: '破褲',
+        description: '夭夭說:「破褲」',
+    },
+    {
+        name: '殺褲',
+        description: '夭夭說:「殺褲」',
+    },
+    {
+        name: '褲致命',
+        description: '夭夭說:「褲致命」',
+    },
+    {
+        name: '褲囚',
+        description: '夭夭說:「褲囚」',
+    },
+    {
+        name: '死亡狙褲',
+        description: '夭夭說:「死亡狙褲」',
+    },
+    {
+        name: '澄空略褲',
+        description: '夭夭說:「澄空略褲」',
+    },
+    {
+        name: '褲池領域',
+        description: '夭夭說:「褲池領域」',
+    },
+  ];
+//#endregion
+
+
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(id), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error);
+
+})();
