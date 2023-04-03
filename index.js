0db888caa3a872f54b0c343c8d3b978b57a29800
AliciaWu <aliciawu0702@gmail.com>
2023-04-03 20:10:31 +0800

    fix bug

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
 const PORT = process.env.PORT || 5001
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   //.set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
